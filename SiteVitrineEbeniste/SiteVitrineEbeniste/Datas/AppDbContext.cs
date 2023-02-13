edb73a0a14349299271bc3b0a6e28a08f2d55c6b
peughouia <peughouia.44@gmail.com>
2023-02-13 19:40:59 +0100

    creation du contexte de la base de données

---
+++ SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiteVitrineEbeniste.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SiteVitrineEbeniste.Datas
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserArticle>().HasKey(us => new
+            {
+                us.UserId,
+                us.ArticleId
+            });
+
+            modelBuilder.Entity<UserArticle>().HasOne(us => us.Viewer).
+                WithMany(viewer => viewer.UserArticles).HasForeignKey(viewer => viewer.UserId);
+            modelBuilder.Entity<UserArticle>().HasOne(us => us.Article).
+                WithMany(article => article.UserArticles).HasForeignKey(article => article.ArticleId);
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<UserArticle> UserArticles { get; set; }
+    }
+}
