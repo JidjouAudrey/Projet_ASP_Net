8ce7fb4c9be2f6f4f444e4075485014223d33d7b
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-02-19 19:15:39 +0100

    Ajout des migrations

---
+++ SiteVitrineEbeniste/SiteVitrineEbeniste/Migrations/20230219174818_KengniFourthMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiteVitrineEbeniste.Migrations
+{
+    public partial class KengniFourthMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
