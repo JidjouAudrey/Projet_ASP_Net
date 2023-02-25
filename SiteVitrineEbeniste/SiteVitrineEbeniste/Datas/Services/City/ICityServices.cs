b91683f8385941927338fc8e35e2a903fb44f2af
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-02-26 00:36:38 +0100

    Création de l'interface des services de city

--- SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/City/ICityServices.cs
+++ SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/City/ICityServices.cs
@@ -2,5 +2,14 @@
 {
     public interface ICityServices
     {
+        Task Add(Models.City city);
+        void Remove(Models.City city);
+        Models.City? Update(int id, Models.City city);
+        IEnumerable<Models.City> GetAll();
+        bool Exists(int id);
+        bool Exists(string name);
+        Models.City? Find(string name);
+        int Count();
+        IEnumerable<Models.User> GetUsers();
     }
 }
