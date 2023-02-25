ad9eb5eee0419d664a8717f8e7d6a81a0cfa6397
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-02-26 00:40:08 +0100

    Ajout des methodes Find(), GetReceivedMessages(), GetSentMessages(), GetPublishedArticles(), GetViewedArticles() et GetComments()

--- SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/User/IUserServices.cs
+++ SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/User/IUserServices.cs
@@ -1,13 +1,20 @@
 ﻿namespace SiteVitrineEbeniste.Datas.Services.User
 {
     public interface IUserServices
-    {
+    { 
         Task Add(Models.User viewer);
         IEnumerable<Models.User> GetAllViewers();
         void Remove(Models.User viewer);
         Models.User? Update(int id, Models.User viewer);
         IEnumerable<Models.User> FilterByUsername(string username);
-        Task<bool> Exists(string username, string password);
+        bool Exists(string email, string password);
         bool Exists(int id);
+        Models.User? Find(string email, string password);
+        int CountViewers();
+        IEnumerable<Models.Message> GetReceivedMessages();
+        IEnumerable<Models.Message> GetSentMessages();
+        IEnumerable<Models.Article> GetPublishedArticles();
+        IEnumerable<Models.Article> GetViewedArticles();
+        IEnumerable<Models.Comment> GetComments();
     }
 }
