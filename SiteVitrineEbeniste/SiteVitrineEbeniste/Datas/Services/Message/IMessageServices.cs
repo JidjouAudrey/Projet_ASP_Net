eafcbb471654a0a404ec42aef4ed74fe8e7f956c
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-02-26 00:37:43 +0100

    création de l'interface des services de message

--- SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/Message/IMessageServices.cs
+++ SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/Message/IMessageServices.cs
@@ -2,5 +2,12 @@
 {
     public interface IMessageServices
     {
+        Task Add(Models.Message message);
+        void Remove(Models.Message message);
+        Models.Message? Update(int id, Models.Message message);
+        IEnumerable<Models.Message> GetAll();
+        bool Exists(int id);
+        bool Exists(int senderId, int receiverId, DateTime sentDate, string content);
+        Models.Message? Find(int senderId, int receiverId, DateTime sentDate, string content);
     }
 }
