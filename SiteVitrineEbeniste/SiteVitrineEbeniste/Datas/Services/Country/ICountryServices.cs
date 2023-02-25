a9355a1663540b2f69a0ba5fb87fa81595d1e4f9
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-02-26 00:37:17 +0100

    création de l'interface des services de country

--- SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/Country/ICountryServices.cs
+++ SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/Country/ICountryServices.cs
@@ -1,6 +1,17 @@
-﻿namespace SiteVitrineEbeniste.Datas.Services.Country
+﻿using SiteVitrineEbeniste.Models;
+
+namespace SiteVitrineEbeniste.Datas.Services.Country
 {
     public interface ICountryServices
     {
+        Task Add(Models.Country country);
+        void Remove(Models.Country country);
+        Models.Country? Update(int id, Models.Country country);
+        IEnumerable<Models.Country> GetAll();
+        bool Exists(int id);
+        bool Exists(string name);
+        Models.Country? Find(string name);
+        int Count();
+        IEnumerable<Models.Country> GetCities();
     }
 }
