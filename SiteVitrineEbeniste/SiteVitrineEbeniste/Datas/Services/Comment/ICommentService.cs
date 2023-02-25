ebbb63c70414aee18f6f77f5a888809bfc283306
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-02-26 00:37:08 +0100

    création de l'interface des services de comment

---
+++ SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/Comment/ICommentService.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace SiteVitrineEbeniste.Datas.Services.Comment
+{
+    public interface ICommentService
+    {
+        Task Add(Models.Comment comment);
+        void Remove(Models.Comment comment);
+        Models.Comment? Update(Models.Comment comment);
+        IEnumerable<Models.Comment> GetAll();
+        Models.Comment? Find(int commenterId, int articleId, DateTime commentedDate);
+        Models.Comment? Find(int commenterId, int articleId, string content);
+        bool Exist(int commenterId, int articleId, DateTime commentedDate);
+    }
+}
