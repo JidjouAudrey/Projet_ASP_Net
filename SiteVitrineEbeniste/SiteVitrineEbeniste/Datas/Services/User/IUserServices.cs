3852973e8d6116ec7840a8049bd598aab874073c
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-02-26 22:02:59 +0100

    Ajout des methodes GetAll(), GetViewedArticles(), GetDIscussion() et GetPublishedArticles()

--- SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/User/IUserServices.cs
+++ SiteVitrineEbeniste/SiteVitrineEbeniste/Datas/Services/User/IUserServices.cs
@@ -3,6 +3,7 @@
     public interface IUserServices
     { 
         Task Add(Models.User viewer);
+        IEnumerable<Models.User> GetAll();
         IEnumerable<Models.User> GetAllViewers();
         void Remove(Models.User viewer);
         Models.User? Update(int id, Models.User viewer);
@@ -11,10 +12,8 @@
         bool Exists(int id);
         Models.User? Find(string email, string password);
         int CountViewers();
-        IEnumerable<Models.Message> GetReceivedMessages();
-        IEnumerable<Models.Message> GetSentMessages();
-        IEnumerable<Models.Article> GetPublishedArticles();
-        IEnumerable<Models.Article> GetViewedArticles();
-        IEnumerable<Models.Comment> GetComments();
+        IEnumerable<Models.UserArticle> GetViewedArticles(int userId);
+        IEnumerable<Models.Message> GetDiscussion(int senderId, int receiverId);
+        IEnumerable<Models.Article> GetPublishedArticles(int publisherId);
     }
 }
